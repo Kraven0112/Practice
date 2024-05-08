88f943e039fb49e9bde53e3f32305b44d005407e
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-05-08 14:38:34 +0545

    added new file

---
+++ Fullstack/Frontend/Language/javascript/core/variable.ts
@@ -0,0 +1,14 @@
+class me{
+    name
+    location
+    age
+    constructor(my)
+    {
+        this.name=my
+        this.location="Bangalore"
+        this.age=20
+    }
+}
+let moon= new String(my){
+console.log(moon)
+}
